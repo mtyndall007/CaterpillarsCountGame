c9a35df4ee9ee72cffae1f3ea49d1d9dc6b2bc86
Nathan Lam <nlam@live.unc.edu>
2019-02-10 16:18:52 -0500

    VERY basic layout
    -Added Background
    -Added Branch
    -Added Caterpillars
    -Added Timer and Score UI
    -Added Scripts that give you points for clicking on bugs

---
+++ CaterpillarsCount/Assets/Scripts/BugClicking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugClicking : MonoBehaviour
+{
+    public GameObject bug;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pointClicked = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D coll = bug.GetComponent<Collider2D>();
+
+            if (coll.OverlapPoint(pointClicked))
+            {
+                ScoreScript.scoreValue += 10;
+            }
+        }
+    }
+}
---
+++ CaterpillarsCount/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] private float startTime = 100;
+    Text uiText;
+    private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText = GetComponent<Text>();
+        currentTime = startTime; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentTime > 0)
+        {
+            currentTime -= 1 * Time.deltaTime;
+
+            uiText.text = "Time: " + currentTime.ToString();
+        }
+
+    }
+}
