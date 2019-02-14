b19a5a68e3b74ed9c6fd41fcaa847cbfa18ea66e
mtyndall007 <mtyndall007@gmail.com>
2019-02-14 17:05:03 -0500

    Added play again functionality
    Play again button replaces the submit button. Resets the scene for now

--- CaterpillarsCount/Assets/Scripts/GameManager.cs
+++ CaterpillarsCount/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 //Script for controlling levels, scores, etc. 
 //Has the UI as a child object
@@ -11,7 +12,9 @@ public class GameManager : MonoBehaviour
 {
 
     private UnityAction submitAction;
+    private UnityAction playAgainAction;
     Button submitButton;
+    Button playAgainButton;
     private int totalScore;
 
     GameObject gameover;
@@ -25,7 +28,7 @@ public class GameManager : MonoBehaviour
 
 
        gameover = GameObject.Find("GameOver");
-        //Make the gameover screen invisible
+       //Make the gameover screen invisible
        gameover.SetActive(false);
     }
 
@@ -49,8 +52,18 @@ public class GameManager : MonoBehaviour
         gameover.SetActive(true);
 
         Text scoreText = GameObject.Find("YourScore").GetComponent<Text>();
-        scoreText.text += totalScore.ToString(); 
+        scoreText.text += totalScore.ToString();
 
+        playAgainButton = GetComponentInChildren<Button>();
+        playAgainAction += PlayAgain;
+        playAgainButton.onClick.AddListener(playAgainAction);
+
+    }
+
+    void PlayAgain()
+    {
+        //Simply resets the scene for now
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     
 }
