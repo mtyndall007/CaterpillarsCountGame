f9d701dc3f0f5dd58a68ca22a4293ae913582022
Nathan Lam <nlam@live.unc.edu>
2019-03-26 17:14:05 -0400

    Update SpawningScript.cs
    Added Comments

--- CaterpillarsCount/Assets/Scripts/SpawningScript.cs
+++ CaterpillarsCount/Assets/Scripts/SpawningScript.cs
@@ -7,23 +7,30 @@ public class SpawningScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int numOfDesiredBugs = 6;
 
-        int removeCount = transform.childCount - numOfDesiredBugs;
+        //Lets you adjust how many bugs you want on that branch
+        int numOfDesiredBugs = 6; 
+
+
+        //keeps track of which bugs already removed
         HashSet<Transform> alreadyRemoved = new HashSet<Transform>();
 
+        //For loop to removed excess bugs
+        int removeCount = transform.childCount - numOfDesiredBugs;
         for (int i = 0; i < removeCount; i++)
         {
-            int childToRemove = Random.Range(0, transform.childCount-1);
-
+            //randomly picks a child to delete
+            int childToRemove = Random.Range(0, transform.childCount - 1);
             Transform child = transform.GetChild(childToRemove);
 
+            //while loop checks and see if that child has already been deleted
             while(alreadyRemoved.Contains(child))
             { 
                 childToRemove = Random.Range(0, transform.childCount - 1);
                 child = transform.GetChild(childToRemove);
             }
 
+            //removes child and adds it to Hashset
             Destroy(child.gameObject);
             alreadyRemoved.Add(child);
 
