8c5955e35a62c34e60fbf25085f1097cfdb3e02a
Nathan Lam <nlam@live.unc.edu>
2019-04-25 15:44:49 -0400

    made prefabs

---
+++ CaterpillarsCount/Assets/Scripts/PrefabMaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabMaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset bugData = Resources.Load<TextAsset>("bugData.csv");
+        Debug.Log("--");
+        Debug.Log(bugData);
+        Debug.Log("--");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
